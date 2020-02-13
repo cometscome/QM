7c09717fa1009862f2435222cf55550801d416ac
cometscome <cometscome@gmail.com>
2020-02-13 11:07:47 +0900

    revised

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,12 @@
 using Documenter
 makedocs(;
-    sitename="Juliaで学ぶ量子力学"
+    sitename="Juliaで学ぶ量子力学",
+    pages = [
+        "Index"=>"index.md",
+        "時間に依存しないシュレーディンガー方程式の解"=>[
+            "ポテンシャルがない場合1次元シュレーディンガー方程式を解き、その後数値的に解いてみる" => "chapter1/01.md",
+            #"About Goma-chan" => "chapter1/goma.md",
+        ],
+#        "Chapter2"=>"chapter2/azarashi.md",
+    ]
 )
